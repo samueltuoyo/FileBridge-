30612749d127818af7f4ad6ffd88395e998b1870
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-05-15 00:51:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const input = document.getElementById("input");
-const click = document.getElementById("click");
 let text = document.getElementById("iptext");
-
-click.addEventListener('click', function check(){
-if(input.value === '') {
+function check(){
+if (input.value === ''){
    text.innerHTML = 'Enter an ip address';
    text.style.color = 'red';
    text.style.fontSize = '20px';
@@ -12,4 +10,4 @@ if(input.value === '') {
 else{
     text.textContent = '';
 }
-});
+}
