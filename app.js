4d079b46bd9f187a94bd0eb1c925319706234910
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-05-26 06:29:09 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const input = document.getElementById("input");
 const text = document.getElementById("iptext");
 const entrance= document.getElementById('main-app');
+const contentElement = document.querySelector('.content');
 function check(){
 if (input.value === ''){
    text.innerHTML = 'Enter an ip address';
    text.style.color = 'red';
    text.style.fontSize = '20px';
 }
-
 else{
     text.textContent = '';
 }
 }
-
 entrance.addEventListener('animationend', () => {
-    entrance.style.display = 'none';
+    window.location.href = 'main.html';
 });
