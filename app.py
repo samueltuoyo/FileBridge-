2248a96ad997a7777c2bc8f332ec73f41c8390e6
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-06-23 20:18:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, session
+from flask_session import Session
+from flask_socketio import SocketIO, join_room, leave_room, emit
+import os 
+import base64
+import socket
+from io import BytesIO
 
 app = Flask(__name__)
+#  This is the config 
+app.config["SECRET_KEY"] = 'This is my Secret Key '
+app.config["SESSION_TYPE"] = 'filesystem'
 
-@app.route('/')
+# This Host is to get the host IP address of the current machine 
+host = socket.gethostbyname(socket.gethostname())
+#host = '192.168.77.115'
+
+Session(app)
+
+DOWNLOAD = os.path.join(app.root_path, 'uploaded_files')
+os.makedirs(DOWNLOAD, exist_ok=True)
+
+SocketIO = SocketIO(app, manage_session=False)
+# These are the routes for the url and radiation page 
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 @app.route('/main')
 def main():
     return render_template('main.html')
@@ -51,7 +72,43 @@ def instructions():
 @app.route('/Downloads')
 def to_downloads():
     return render_template('Downloads/Downloads.html')
-            
 
+# This is the code for the socketIO connection 
+
+@SocketIO.on('sender',  namespace='/sender')
+def sender(message):
+    Room_Name: str = session.get('Room_Name')
+    username: str = session.get('username')
+    join_room(room=Room_Name)
+    emit('status', {
+        "msg": f"{username} has joined the room!!! "
+    }, room=Room_Name)
+
+
+@SocketIO.on("text", namespace="/sender")
+def text(message):
+    Room_Name = session.get("Room_Name")
+    username = session.get("username")
+    emit("message", {"msg": f"{username}: {message['msg']}"}, room=Room_Name)
+
+
+@SocketIO.on('left', namespace='/sender')
+def left(message):
+    Room_Name: str = session.get("Room_Name")
+    username: str = session.get("username")
+    leave_room(room=Room_Name)
+    session.clear()
+    emit("status", {"msg": f"{username}: has left the room :("}, room=Room_Name)
+
+@SocketIO.on('file', namespace='/sender')
+def handle_file(data):
+    room = session.get('Room_Name')
+    file_data = data['file']
+    file_name = data['fileName']
+    emit('message', {
+        'file': file_data,
+        'fileName': file_name,
+        'username': session.get('username')
+    }, room=room)
 if __name__ == '__main__':
     app.run(debug=True)
