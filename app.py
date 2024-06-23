75d195a1b536942fa145ee3bc04f42817213e0a2
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-06-23 17:59:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ from io import BytesIO
 
 app = Flask(__name__)
 #  This is the config 
-app.config["SECRET_KEY"] = 'This is my Secret Key'
+app.config["SECRET_KEY"] = 'This is my Secret Key '
 app.config["SESSION_TYPE"] = 'filesystem'
 
 # This Host is to get the host IP address of the current machine 
-host = "192.168.221.1"
+host = socket.gethostbyname(socket.gethostname())
+#host = '192.168.77.115'
 
 Session(app)
 
@@ -39,6 +40,10 @@ def sender():
      # the tab or open a new tab
         session["username"] = username
         session["Room_Name"] = room_name
+        # This is almost like the required filed
+        # if username and room_name == "0":
+        #     return "<h1> Error 404 </h1>"
+
         return render_template('share/sender.html', Session=session)
     # Else the user is already logged in and the client is already in a room it will redirect back to the room 
     else: 
@@ -55,19 +60,18 @@ def receiver():
 @app.route('/download_temp/<file_name>')
 def download_temp_file(file_name):
     file_data = request.args.get('file_data')
-    file_bytes = base64.b64decode(file_data.split(",")[1])
+    file_bytes = base64.b64decode(file_data.split(",")[0])
     file_stream = BytesIO(file_bytes)
     file_stream.seek(0)
     return send_file(file_stream, as_attachment=True, download_name=file_name)
-
 
 @app.route('/instructions')
 def instructions():
     return render_template('instructions/instructions.html')
 
-@app.route('/mp3')
-def to_mp3():
-    return render_template('To-mp3/video-to-mp3.html')
+@app.route('/Downloads')
+def to_downloads():
+    return render_template('Downloads/Downloads.html')
 
 # This is the code for the socketIO connection 
 
@@ -83,39 +87,34 @@ def sender(message):
 
 @SocketIO.on("text", namespace="/sender")
 def text(message):
-    Room_Name = session.get("room_name")
+    Room_Name = session.get("Room_Name")
     username = session.get("username")
     emit("message", {"msg": f"{username}: {message['msg']}"}, room=Room_Name)
 
 
 @SocketIO.on('left', namespace='/sender')
 def left(message):
-    Room_Name: str = session.get("room_name")
+    Room_Name: str = session.get("Room_Name")
     username: str = session.get("username")
     leave_room(room=Room_Name)
-    session.clear
+    session.clear()
     emit("status", {"msg": f"{username}: has left the room :("}, room=Room_Name)
 
 @SocketIO.on('file', namespace='/sender')
 def handle_file(data):
     room = session.get('Room_Name')
-    file_data = data['file'].split(",")[1]  # Extract base64 data
+    file_data = data['file']
     file_name = data['fileName']
-    file_bytes = base64.b64decode(file_data)
-    file_stream = BytesIO(file_bytes)
-    file_stream.seek(0)
-    
-    # Emit a message with a link to download the file
-    file_url = url_for('download_temp_file', file_name=file_name, _external=True)
     emit('message', {
-        'file': file_url,
+        'file': file_data,
         'fileName': file_name,
-        'fileType': data['fileType'],
         'username': session.get('username')
     }, room=room)
+
 
 
 # This is for the running 
 if __name__ == '__main__':
     app.secret_key = "SECRET"
     app.run(debug=True, host=host, port=2024)
+  #  SocketIO.run()
