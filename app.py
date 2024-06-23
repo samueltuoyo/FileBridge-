0cca17330b1cda52364ad2e030cb8f2fbd0276e5
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-06-23 19:18:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
-
+from flask_session import Session
+from flask_socketio import SocketIO, join_room, leave_room, emit
+import os 
+import base64
+import socket
+from io import BytesIO
 app = Flask(__name__)
 
 @app.route('/')
