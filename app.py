fa0c9b3cfe0f3bbfb9c20928bbd19225efd45cd5
KINGSLEY ESISI <kingsleyesisi@outlook.com>
2024-06-30 00:12:06 +0100

    Commit 30/06/2024

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request, url_for, redirect, session, send_file
-#from flask_session import Session
-#from flask_socketio import SocketIO, join_room, leave_room, emit
-#import os
-#import base64
-#import socket
-#from io import BytesIO
+from flask_session import Session
+from flask_socketio import SocketIO, join_room, leave_room, emit
+import os
+import base64
+import socket
+from io import BytesIO
 
 app = Flask(__name__)
-#app.config["SECRET_KEY"] = 'This is my Secret Key '
-#app.config["SESSION_TYPE"] = 'filesystem'
+app.config["SECRET_KEY"] = 'This is my Secret Key '
+app.config["SESSION_TYPE"] = 'filesystem'
 
 host = socket.gethostbyname(socket.gethostname())
-# host = "192.168.238.1"
+# host = "wifi ip_address here"
 Session(app)
 
 # DOWNLOAD = os.path.join(app.root_path, 'uploaded_files')
 # os.makedirs(DOWNLOAD, exist_ok=True)
 
-#socketio = SocketIO(app, manage_session=False)
+socketio = SocketIO(app, manage_session=False)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -39,6 +39,9 @@ def sender():
         return render_template('share/receiver.html', Session=session)
     else:
         return redirect(url_for('receiver'))
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/receiver', methods=["GET", "POST"])
 def receiver():
@@ -60,41 +63,41 @@ def instructions():
 def to_downloads():
     return render_template('Downloads/Downloads.html')
 
-#@socketio.on('sender', namespace='/sender')
-#def sender_event(message):
-   # Room_Name = session.get('Room_Name')
-   # username = session.get('username')
-  #  join_room(Room_Name)
-   # emit('status', {
-  #      "msg": f"{username} has joined the room!!!"
-   # }, room=Room_Name)
+@socketio.on('sender', namespace='/sender')
+def sender_event(message):
+   Room_Name = session.get('Room_Name')
+   username = session.get('username')
+   join_room(Room_Name)
+   emit('status', {
+       "msg": f"{username} has joined the room!!!"
+   }, room=Room_Name)
 
-#@socketio.on('text', namespace='/sender')
-#def text_event(message):
-    #Room_Name = session.get('Room_Name')
-    #username = session.get('username')
-   # emit('message', {
-   #     "msg": f"{username}: {message['msg']}"
-   # }, room=Room_Name)
+@socketio.on('text', namespace='/sender')
+def text_event(message):
+    Room_Name = session.get('Room_Name')
+    username = session.get('username')
+    emit('message', {
+       "msg": f"{username}: {message['msg']}"
+   }, room=Room_Name)
 
-#@socketio.on('left', namespace='/sender')
-#def left_event(message):
-    #Room_Name = session.get('Room_Name')
-  #  username = session.get('username')
-    #leave_room(Room_Name)
-   # session.clear()
-  #  emit('status', {
-    #    "msg": f"{username} has left the room :("
-  #  }, room=Room_Name)
+@socketio.on('left', namespace='/sender')
+def left_event(message):
+    Room_Name = session.get('Room_Name')
+    username = session.get('username')
+    leave_room(Room_Name)
+    session.clear()
+    emit('status', {
+       "msg": f"{username} has left the room :("
+   }, room=Room_Name)
 
-#@socketio.on('file', namespace='/sender')
-#def handle_file(data):
-#    Room_Name = session.get('Room_Name')
-  #  emit('message', {
-     #   'file': data['file'],
-     #   'fileName': data['fileName'],
-       # 'username': session.get('username')
-    #}, room=Room_Name)
+@socketio.on('file', namespace='/sender')
+def handle_file(data):
+   Room_Name = session.get('Room_Name')
+   emit('message', {
+       'file': data['file'],
+       'fileName': data['fileName'],
+       'username': session.get('username')
+    }, room=Room_Name)
 
 if __name__ == '__main__':
     socketio.run(app, debug="True", port="2024")
