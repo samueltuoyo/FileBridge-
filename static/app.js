9011d8bb9ee9d78b92724b275201a67781991a2a
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-07-09 18:27:28 +0100

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -1,26 +1,24 @@
+const entrance = document.getElementById('main-app');
 const inputField1 = document.getElementById('username');
-const inputField2 = document.getElementById('Room_Name')
-const text = document.getElementById("iptext");
+const inputField2 = document.getElementById('Room_Name');
+const text = document.getElementById('iptext');
 const contentElement = document.querySelector('.content');
-const currentPage = window.location.pathname;
-const links = document.querySelectorAll('#footer a');
+const btn = document.querySelector('#submit-btn'); 
+
 function check(){
   if (inputField1.value === '' || inputField2.value === ''){
-    text.innerHTML = 'Please Create a room or join || create a room number';
+    text.textContent = 'Please Create a room or join || create a room number';
     text.style.color = 'red';
     text.style.fontSize = '20px';
+    btn.disabled = true;
     
   } else{
     text.textContent = '';
+    text.style.color = 'none';
+    text.style.fontSize = 'none';
+    btn.disabled = false;
   }
-  
-  inputField1.addEventListener('input', check);
-inputField2.addEventListener('input', check);
 }
-links.forEach(link => {
-    if (link.href.includes(currentPage)) {
-      link.classList.add('active');
-    }
-  });
 
+btn.addEventListener('click', check);
 
