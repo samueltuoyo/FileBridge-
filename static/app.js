b98fa1785bf3ee27d59b3091760f15474b2af0fe
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-07-13 09:48:54 +0100

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -1,24 +1,9 @@
-const entrance = document.getElementById('main-app');
-const inputField1 = document.getElementById('username');
-const inputField2 = document.getElementById('Room_Name');
-const text = document.getElementById('iptext');
-const contentElement = document.querySelector('.content');
-const btn = document.querySelector('#submit-btn'); 
-
-function check(){
-  if (inputField1.value === '' || inputField2.value === ''){
-    text.textContent = 'Please Create a room or join || create a room number';
-    text.style.color = 'red';
-    text.style.fontSize = '20px';
-    btn.disabled = true;
-    
-  } else{
-    text.textContent = '';
-    text.style.color = 'none';
-    text.style.fontSize = 'none';
-    btn.disabled = false;
-  }
-}
-
-btn.addEventListener('click', check);
-
+const mobNavLinks = document.querySelector('#footer div');
+mobNavLinks.forEach((link) => {
+   link.addEventListener('click', () => {
+   mobNavLinks.forEach((others) => {
+       others.classList.remove('active');
+   })
+       link.classList.toggle('active');
+   })
+});
