7129fd45bf29e6dcbd4bd3272ae33b85ff18a8fa
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2024-07-13 12:58:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,10 @@ def index():
 def main():
     return render_template('main.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/sender', methods=["GET", "POST"])
 def sender():
     if request.method == "POST":
